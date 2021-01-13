b7b3a1b9f24522affff70ab596f9fa0deb6642b8
pattywagonpbj <patrick.jarrett11@gmail.com>
2021-01-13 14:21:34 -0800

    update problem two

--- js/app.js
+++ js/app.js
@@ -30,11 +30,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  let product = a * b;
+  let message = `The product of ${a} and ${b} is ${product}.`;
+  return[product, message];
+  // consule.log([product, message]);
 }
 
 // Here is the test for multiply(); uncomment it to run it
-//testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -51,7 +54,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-var test = 0;
+
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
